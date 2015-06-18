ae6897d345ee1a2c8661e4e380aaf42b435c1522
Ryan Drew <rsdrew@gmail.com>
2015-06-18 15:44:19 -0400

    A magic 8-ball command.

---
+++ plugins/eightball.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+module.exports = function (bot) {
+    var resps = [];
+
+    fs.readFile('./plugins/eightball.txt', encoding='UTF-8', function (err, data) {
+        if (err) throw err;
+        resps = data.split('\n');
+        if (resps[resps.length - 1] === '') resps.pop();
+        console.log(resps);
+    });
+
+    bot.addCommand('8ball', function (cmd) {
+        bot.say(cmd.network, cmd.replyto, resps[Math.floor(Math.random() * resps.length)]);
+    });
+};
