291eac0aed297bc58ebd529914412b8937a0b3ee
saltire sable <x@saltiresable.com>
2015-06-22 17:17:02 -0400

    Proof of concept for unit testing plugins

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
+
+gulp.task('test', function () {
+    return gulp.src('./test/spec/*.js')
+        .pipe(jasmine());
+});
--- plugins/echo.js
+++ plugins/echo.js
@@ -1,6 +1,6 @@
 // simple echo plugin for testing purposes
 module.exports = function () {
-    this.listen('message', '.*', function (msg) {
+    this.listen('message', '.*', function repeatMessage(msg) {
         this.say(msg.network, msg.replyto, msg.text);
-    }, true);
+    });
 };
