c7e8836d3f1dc61b942af9d7bc3216f8335ea948
saltire sable <x@saltiresable.com>
2015-06-25 11:08:55 -0400

    Handle file error in 8-ball plugin

--- plugins/eightball.js
+++ plugins/eightball.js
@@ -6,12 +6,12 @@ module.exports = function (bot) {
     var filename = bot.config.get('plugins.eightball.response_file', './plugins/data/eightball.txt');
 
     fs.readFile(filename, {encoding: 'UTF-8'}, function (err, data) {
-        if (err) throw err;
-        resps = data.split('\n');
-        if (resps[resps.length - 1] === '') resps.pop();
+        if (err) return bot.error("Couldn't find 8-ball response file.");
+        resps = data.trim().split('\n');
     });
 
     bot.addCommand(['8', '8ball', 'eightball'], function (cmd) {
+        if (!resps.length) return;
         bot.say(cmd.network, cmd.replyto, resps[Math.floor(Math.random() * resps.length)]);
     });
 };
