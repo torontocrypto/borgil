f4376e4fa9fcfda1c9af256c2cfacaae4ced2d62
saltire sable <saltiresable@gmail.com>
2017-09-11 17:42:40 -0400

    Move some functionality into bot core; update transports to ES6

--- bot/bot.js
+++ bot/bot.js
@@ -1,10 +1,24 @@
 'use strict';
 
 const EventEmitter = require('eventemitter2').EventEmitter2;
+const fs = require('fs');
+const handlebars = require('handlebars');
+const moment = require('moment-timezone');
+const path = require('path');
+const winston = require('winston');
 
 const Config = require('./config');
 const Plugin = require('./plugin');
 
+
+const logDefaults = {
+    dir: 'logs',
+    filename_template: 'bot--{{date}}.log',
+    date_format: 'YYYY-MM-DD--HH-mm-ss',
+    console: false,
+    debug: false,
+};
+const defaultBuffer = 100;
 
 module.exports = class Bot extends EventEmitter {
     constructor(configfile) {
@@ -12,15 +26,106 @@ module.exports = class Bot extends EventEmitter {
 
         this.config = new Config(configfile);
 
-        this.plugins = {};
         this.memory = {};
 
         // Include extra functionality.
-        require('./log')(this);
-        require('./transports')(this);
-        require('./buffers')(this);
+        this.initLog();
+        this.initBuffers();
+        this.initTransports();
+        this.initPlugins();
+    }
 
-        // Activate all plugins mentioned in the plugins section of the config.
+    initLog() {
+        const level = this.config.get('log.debug') ? 'debug' : 'info';
+        const renderFilename = handlebars.compile(
+            this.config.get('log.filename_template', logDefaults.filename_template));
+
+        const logdir = this.config.get('log.dir', logDefaults.dir);
+        try {
+            fs.mkdirSync(logdir);
+        }
+        catch (err) {
+            if (err.code !== 'EEXIST') throw err;
+        }
+
+        const dateFormat = this.config.get('log.date_format', logDefaults.dateFormat);
+        const timezone = this.config.get('log.timezone');
+        const logfile = path.join(logdir, renderFilename({
+            date: (timezone ? moment.tz(timezone) : moment()).format(dateFormat),
+        }));
+
+        const transports = [];
+        if (logfile) {
+            transports.push(new winston.transports.File({
+                filename: logfile,
+                json: false,
+                level,
+                timestamp: true,
+            }));
+        }
+        if (this.config.get('log.console')) {
+            transports.push(new winston.transports.Console({
+                colorize: true,
+                level,
+                timestamp: true,
+            }));
+        }
+
+        this.log = new winston.Logger({
+            transports,
+        });
+    }
+
+    initBuffers() {
+        // Create buffer objects for each client.
+        this.buffers = {};
+
+        // Log each message to a buffer.
+        this.on('message', (transport, msg) => {
+            // Initialize buffer for this transport and source if necessary.
+            if (!(transport.name in this.buffers)) {
+                this.buffers[transport.name] = {};
+            }
+            if (!(msg.replyto in this.buffers[transport.name])) {
+                this.buffers[transport.name][msg.replyto] = [];
+            }
+            const buffer = this.buffers[transport.name][msg.replyto];
+
+            // Trim buffer to maximum length, then add this message.
+            if (buffer.length >= this.config.get('buffer', defaultBuffer)) {
+                buffer.pop();
+            }
+            buffer.unshift(msg);
+        });
+    }
+
+    // Activate all transports mentioned in the transports section of the config.
+    initTransports() {
+        const tpConfigs = this.config.get('transports', {});
+
+        this.transports = {};
+        Object.keys(tpConfigs).forEach((tpName) => {
+            this.log.info('Activating transport:', tpName);
+            const tpConfig = tpConfigs[tpName];
+            let transport;
+            try {
+                // eslint-disable-next-line global-require
+                const TpType = require(`./transports/${tpConfig.type}`);
+                transport = new TpType(this, tpName, tpConfig);
+            }
+            catch (err) {
+                this.log.warn('Error activating transport %s:', tpName, err.message);
+                return;
+            }
+            this.transports[tpName] = transport;
+        });
+
+        this.log.info(Object.keys(this.transports).length, 'transports(s) activated.');
+    }
+
+    // Activate all plugins mentioned in the plugins section of the config.
+    initPlugins() {
+        this.plugins = {};
         Object.keys(this.config.get('plugins', {})).forEach((pluginName) => {
             this.log.info('Activating plugin:', pluginName);
             let plugin;
@@ -33,5 +138,7 @@ module.exports = class Bot extends EventEmitter {
             }
             this.plugins[pluginName] = plugin;
         });
+
+        this.log.info(Object.keys(this.plugins).length, 'plugin(s) activated.');
     }
 };
--- bot/transports/telegram.js
+++ bot/transports/telegram.js
@@ -1,144 +1,142 @@
-var request = require('request');
-var util = require('util');
+'use strict';
 
-var Transport = require('./transport');
+const request = require('request');
+const util = require('util');
+
+const Transport = require('../transport');
 
 
-var Telegram = module.exports = function (bot, name, config) {
-    Transport.call(this, bot, name);
+module.exports = class Telegram extends Transport {
+    constructor(bot, name, config) {
+        super(bot, name);
 
-    this.token = config.token;
-    this.offset = 0;
+        this.token = config.token;
+        this.offset = 0;
 
-    this._getUpdates();
-};
-util.inherits(Telegram, Transport);
-
-Telegram.prototype._send = function (method, params, callback) {
-    request({
-        uri: 'https://api.telegram.org/bot' + this.token + '/' + method,
-        method: 'post',
-        json: true,
-        qs: params
-    }, function (err, res, body) {
-        if (err) return callback ? callback(err) : null;
-        if (!body.ok || !body.result) {
-            var err = new Error(body.description || 'Unknown API error');
-            err.status = body.error_code || 400;
-            return callback ? callback(err) : null;
-        }
-
-        if (callback) callback(null, body.result);
-    });
-}
-
-Telegram.prototype._getUpdates = function () {
-    var transport = this;
-
-    var debug = this.bot.config.get('log.debug');
-
-    if (debug) {
-        this.bot.log.debug('%s: Polling for Telegram updates...', this.name);
+        this._getUpdates();
     }
 
-    // Make a long polling call to the API.
-    this._send('getUpdates', {
-        offset: this.offset,
-        timeout: 20
-    }, function (err, result) {
-        if (err) {
-            transport.emit('error', err.message);
-        }
-        else {
-            // Emit events for any messages received.
-            (result || []).forEach(function (update) {
-                if (debug) {
-                    transport.bot.log.debug('%s: Got update:', transport.name, JSON.stringify(update));
+    _send(method, params, callback) {
+        request(
+            {
+                uri: `https://api.telegram.org/bot${this.token}/${method}`,
+                method: 'post',
+                json: true,
+                qs: params,
+            },
+            (err, res, body) => {
+                if (err) {
+                    return callback ? callback(err) : null;
+                }
+                if (!body.ok || !body.result) {
+                    const err = new Error(body.description || 'Unknown API error');
+                    err.status = body.error_code || 400;
+                    return callback ? callback(err) : null;
                 }
 
-                transport.offset = Math.max(transport.offset, update.update_id + 1);
-
-                if (update.message) {
-                    transport._parseMessage(update.message);
-                }
-                if (update.channel_post) {
-                    transport._parseChannelPost(update.channel_post);
+                if (callback) {
+                    callback(null, body.result);
                 }
             });
+    }
+
+    _getUpdates() {
+        this.bot.log.debug('%s: Polling for Telegram updates...', this.name);
+
+        // Make a long polling call to the API.
+        this._send('getUpdates',
+            {
+                offset: this.offset,
+                timeout: 20,
+            },
+            (err, result) => {
+                if (err) {
+                    this.emit('error', err.message);
+                }
+                else {
+                    // Emit events for any messages received.
+                    (result || []).forEach((update) => {
+                        this.bot.log.debug('%s: Got update:', this.name, JSON.stringify(update));
+
+                        this.offset = Math.max(this.offset, update.update_id + 1);
+
+                        if (update.message) {
+                            this._parseMessage(update.message);
+                        }
+                        if (update.channel_post) {
+                            this._parseChannelPost(update.channel_post);
+                        }
+                    });
+                }
+
+                // Continue polling.
+                this._getUpdates();
+            });
+    }
+
+    _parseMessage(message) {
+        const fromName = message.from.first_name +
+            (message.from.last_name ? ` ${message.from.last_name}` : '');
+
+        const data = {
+            from: message.from.username || message.from.id,
+            from_name: fromName,
+            to: message.chat.id,
+            replyto: message.chat.id,
+            replyto_name: message.chat.title || fromName,
+            text: message.text || '',
+            time: new Date(message.date * 1000),
+        };
+
+        // If this message is a command, add command properties and emit a command event.
+        // Currently this uses a slash instead of the configured command character.
+        const cmd = (message.entities || [])
+            .find(entity => (entity.type === 'bot_command' && entity.offset === 0));
+        if (cmd) {
+            data.command = message.text.slice(cmd.offset + 1, cmd.length);
+            data.args = message.text.slice(cmd.offset + cmd.length).trim();
+            this.emit('command', data);
         }
 
-        // Continue polling.
-        transport._getUpdates();
-    });
-};
-
-Telegram.prototype._parseMessage = function (message) {
-    var from_name = message.from.first_name +
-        (message.from.last_name ? ' ' + message.from.last_name : '');
-
-    var data = {
-        from: message.from.username || message.from.id,
-        from_name: from_name,
-        to: message.chat.id,
-        replyto: message.chat.id,
-        replyto_name: message.chat.title || from_name,
-        text: message.text || '',
-        time: new Date(message.date * 1000),
-    };
-
-    // If this message is a command, add command properties and emit a command event.
-    // Currently this uses a slash instead of the configured command character.
-    var cmd = (message.entities || []).find(function (entity) {
-        return entity.type === 'bot_command' && entity.offset === 0;
-    });
-    if (cmd) {
-        data.command = message.text.slice(cmd.offset + 1, cmd.length);
-        data.args = message.text.slice(cmd.offset + cmd.length).trim();
-        this.emit('command', data);
+        this.emit('message', data);
     }
 
-    this.emit('message', data);
-};
+    _parseChannelPost(post) {
+        // TODO: Telegram channel posts don't expose sending user, but come from the channel itself.
+        // Most plugins assume a sending user, so don't emit message/command events for now.
+        // Instead let's emit 'broadcast' events, but maybe remove these in the future
+        // if plugins are updated to handle them as regular message events.
 
-Telegram.prototype._parseChannelPost = function (post) {
-    // TODO: Telegram channel posts don't expose a sending user, but come from the channel itself.
-    // Most plugins assume a sending user, so don't emit message/command events for now.
-    // Instead let's emit 'broadcast' events, but maybe remove these in the future
-    // if plugins are updated to handle them as regular message events.
+        const fromName = post.chat.title || post.chat.username;
 
-    var from_name = post.chat.title || post.chat.username;
+        const data = {
+            from: post.chat.username || post.chat.id,
+            from_name: fromName,
+            text: post.text || '',
+            time: new Date(post.date * 1000),
+        };
 
-    var data = {
-        from: post.chat.username || post.chat.id,
-        from_name: from_name,
-        text: post.text || '',
-        time: new Date(post.date * 1000),
-    };
+        // If this message is a command, add command properties and emit a command event.
+        // Currently this uses a slash instead of the configured command character.
+        const cmd = (post.entities || [])
+            .find(entity => (entity.type === 'bot_command' && entity.offset === 0));
+        if (cmd) {
+            data.command = post.text.slice(cmd.offset + 1, cmd.length);
+            data.args = post.text.slice(cmd.offset + cmd.length).trim();
+            this.emit('broadcastcommand', data);
+        }
 
-    // If this message is a command, add command properties and emit a command event.
-    // Currently this uses a slash instead of the configured command character.
-    var cmd = (post.entities || []).find(function (entity) {
-        return entity.type === 'bot_command' && entity.offset === 0;
-    });
-    if (cmd) {
-        data.command = post.text.slice(cmd.offset + 1, cmd.length);
-        data.args = post.text.slice(cmd.offset + cmd.length).trim();
-        this.emit('broadcastcommand', data);
+        this.emit('broadcast', data);
     }
 
-    this.emit('broadcast', data);
-};
-
-Telegram.prototype.say = function (target) {
-    var text = util.format.apply(null, Array.prototype.slice.call(arguments, 1));
-    this._send('sendMessage', {
-        chat_id: target,
-        text: text,
-    });
-};
-
-Object.defineProperty(Telegram.prototype, 'channels', {
-    get: function () {
-        return this.bot.config.get('transports.' + this.name + '.chat_ids', []);
+    say(target, ...args) {
+        this._send('sendMessage', {
+            chat_id: target,
+            text: util.format(...args),
+        });
     }
-});
+
+    get channels() {
+        return this.bot.config.get(`transports.${this.name}.chat_ids`, []);
+    }
+};
